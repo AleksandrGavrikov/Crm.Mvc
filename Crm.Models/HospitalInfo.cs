2b92870bb362e74a6c11cf69e0a4725df5c749ea
AleksandrGavrikov <Sasha3310@yandex.ru>
2022-11-24 21:33:15 +0500

    added DbSet

--- Crm.Models/HospitalInfo.cs
+++ Crm.Models/HospitalInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Crm.Models
 {
-    public class Hospital
+    public class HospitalInfo
     {
         public int Id { get; set; }
         public string Name { get; set; }
