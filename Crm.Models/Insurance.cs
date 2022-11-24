6d67801a838dcd572b814a06627486aa2cb01bc6
AleksandrGavrikov <Sasha3310@yandex.ru>
2022-11-24 21:11:56 +0500

    added Crm.Models

---
+++ Crm.Models/Insurance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Models
+{
+    public class Insurance
+    {
+        public int Id { get; set; }
+        public string PoliceNumber { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public ICollection<Bill> Bill { get; set; }
+    }
+}
